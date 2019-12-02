e52d996e2f7807826e42435c5e9b0a341f194bb4
Qu <Quan0027@elec.techcollege.dk>
2019-12-02 14:24:19 +0100

    Exercise 1 in classes

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,28 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
+            var stopwatch = new Stopwatch();
+
+            Console.WriteLine("Press 1 to start and 2 to stop watch");
+
+            if (Console.ReadLine() == "1")
+            {
+                stopwatch.Start();
+            }
+            
+            
+            Console.WriteLine("Hit 2 to stop watch");
+
+            if (Console.ReadLine() == "2")
+            {
+                stopwatch.Stop();
+            }
+            
+
+            
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public enum WatchState
+        {
+            Start,
+            Stop
+        }
+       
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeStop { get; set; }
+
+        public void Start()
+        {
+            TimeStart = DateTime.Now;
+            Console.WriteLine($"Stopwatch started at {TimeStart}\n");
+        }
+
+        public void Stop()
+        {
+            TimeStop = DateTime.Now;
+            Console.WriteLine($"Stopwatch stop at {TimeStop}\n");
+        }
+    }
+}
