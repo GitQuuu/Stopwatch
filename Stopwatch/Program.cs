8ce32590c974c766a06749f945f6bb3f26df6b6d
Qu <Quan0027@elec.techcollege.dk>
2019-12-05 14:54:35 +0100

    Refactor cleaner code

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -12,61 +12,8 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            bool stopwatchLoop = true;
-
-            while (stopwatchLoop == true)
-                
-            {
-                bool exitOrContinue = true;
-                Console.WriteLine("Press 1 to start and 2 to stop watch");
-
-
-                    switch (int.TryParse(Console.ReadLine(), out int value) ? value : 0)
-                    {
-                        case (int)Stopwatch.WatchState.On:
-                            Console.Clear();
-                            Stopwatch.StartTimer();
-                            continue;
-                        case (int) Stopwatch.WatchState.Off:
-                            Stopwatch.StopTimer();
-                            break;
-                        default:
-                            Console.WriteLine("Please try again");
-                            continue;
-                    }
-
-                while (exitOrContinue)
-                {
-                    Console.WriteLine("Continue program y/n?");
-
-                    bool doubleCheckExit = true;
-                    while (doubleCheckExit)
-                    {
-                        switch (char.TryParse(Console.ReadLine(),out char yesOrNo) ? yesOrNo : default )
-                        {
-                            case (char)Stopwatch.Choices.Yes:
-
-                                doubleCheckExit = false;
-                                stopwatchLoop = true;
-                                exitOrContinue = false;
-
-                                break;
-                            case (char)Stopwatch.Choices.No:
-
-                                doubleCheckExit = false;
-                                exitOrContinue = false;
-                                stopwatchLoop = false;
-
-                                break;
-                            default:
-                                Console.Write("Please select either y or n to continue program\n");
-                                break;
-                        }
-                      
-                    }
-                    
-                }
-            }
+            Stopwatch.PowerOn("Press 1 to start and 2 to stop the Stopwatch");
+            Stopwatch.PowerOff("Continue program y/n?");
 
         }
     }
--- Stopwatch/Stopwatch.cs
+++ Stopwatch/Stopwatch.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Runtime.CompilerServices;
 
 namespace Stopwatch
 {
-    public static class Stopwatch
+    public class Stopwatch
     {
+        private static bool _loopState;
+
         public enum Choices
         {
             Yes = 'y',
@@ -22,6 +25,66 @@ namespace Stopwatch
         public static DateTime TimeStart { get; private set; }
         public static DateTime TimeStop { get; private set; }
 
+
+        public static void PowerOn(string introduction)
+        {
+            bool LoopState = true;
+            while (LoopState)
+            {
+                Console.WriteLine(introduction);
+
+                switch (int.TryParse(Console.ReadLine(), out int value) ? value : 0)
+                {
+                    case (int)Stopwatch.WatchState.On:
+                        Console.Clear();
+                        Stopwatch.StartTimer();
+                        continue;
+                    case (int)Stopwatch.WatchState.Off:
+                        Stopwatch.StopTimer();
+                        LoopState = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose either 1 or 2");
+                        continue;
+                }
+
+            }
+        }
+
+        public static void PowerOff(string actions)
+        {
+            bool powerOffState = true;
+            while (powerOffState)
+            {
+                bool inputCheck = true;
+                while (inputCheck)
+                {
+                    Console.WriteLine(actions);
+                    switch (char.TryParse(Console.ReadLine(), out char yesOrNo) ? yesOrNo : default)
+                    {
+                        case (char)Stopwatch.Choices.Yes:
+
+                            inputCheck = false;
+                            powerOffState = true;
+                            PowerOn("Press 1 to start again and 2 to end program");
+
+                            break;
+                        case (char)Stopwatch.Choices.No:
+
+                            inputCheck = false;
+                            powerOffState = false;
+                            break;
+                        default:
+                            Console.Write("Please select either y or n to continue program\n");
+                            break;
+                    }
+
+                }
+
+            }
+        }
+
+
         public static void StartTimer()
         {
             TimeStart = DateTime.Now;
@@ -35,9 +98,8 @@ namespace Stopwatch
                 if (Console.ReadLine() == "2")
                 {
                     StopTimer();
-                    preventOverlap = false;
-
                     Console.WriteLine($"Runtime is {Stopwatch.Duration.TotalSeconds} seconds \n");
+                    preventOverlap = false;
                 }
                 else
                 {
@@ -50,6 +112,7 @@ namespace Stopwatch
 
         public static void StopTimer()
         {
+            
             TimeStop = DateTime.Now;
             Duration = TimeStop - TimeStart;   
             Console.WriteLine($"Stopwatch stop at {TimeStop}\n");
